dcac7308205f93b68f3bdea6f78e42d949566ae1
Fxr-Whxt <aleksandrvelickin0@gmail.com>
2025-03-19 21:58:48 +0100

    new kata added to a folder

---
+++ Python/You only need one - Beginner.py
@@ -0,0 +1,16 @@
+'''You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+a can contain numbers or strings. x can be either.
+
+Return true if the array contains the value, false if not.
+'''
+
+# Given Code :
+
+def check(seq, elem):
+    pass
+
+# Solution :
+
+def check(seq, elem):
+    return elem in seq
